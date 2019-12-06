88f7536ad2352487ddb9757500a09f9614ef0eb9
Kristián Mičko <kristian.micko@student.tuke.sk>
2019-12-06 22:23:10 +0100

    osmeRestNaCloude

--- Miestnost/index.php
+++ Miestnost/index.php
@@ -72,15 +72,15 @@ $method = $_SERVER['REQUEST_METHOD'];
 include("pripojenie.php");
 $miestnost = new Miestnost();
 if ($method == 'GET') {
-  //if(!isset($url_array[1])){
+  if(!isset($url_array[0])){
     $data = $miestnost->getData();
-    $data = $url_array[0];
+    //$data = $url_array[0];
     $response['status'] = 200;
     $response['data'] = $data;
 
 
-  /*}else{
-      $id=$url_array[1];
+  }else{
+      $id=$url_array[0];
 			$data=$miestnost->getDataById($id);
 			if(empty($data)) {
 				$response['status'] = 404;
@@ -88,7 +88,7 @@ if ($method == 'GET') {
 			}else{
 				$response['status'] = 200;
 				$response['data'] = $data;
-  }*/
+  }
 }else if($method == 'POST' ){
   $json = $_POST['miestnost'];
   //$post = json_decode($json);
