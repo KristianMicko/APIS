2bc408276506e105ad19ea6a79e14f1a06570cbf
Kristián Mičko <kristian.micko@student.tuke.sk>
2019-12-06 10:51:48 +0100

    tretieRestNaCloude

--- polozky/index.php
+++ polozky/index.php
@@ -1,71 +1,22 @@
 <?php
-function deliver_response($response){
-	// Define HTTP responses
-	$http_response_code = array(
-		100 => 'Continue',
-		101 => 'Switching Protocols',
-		200 => 'OK',
-		201 => 'Created',
-		202 => 'Accepted',
-		203 => 'Non-Authoritative Information',
-		204 => 'No Content',
-		205 => 'Reset Content',
-		206 => 'Partial Content',
-		300 => 'Multiple Choices',
-		301 => 'Moved Permanently',
-		302 => 'Found',
-		303 => 'See Other',
-		304 => 'Not Modified',
-		305 => 'Use Proxy',
-		306 => '(Unused)',
-		307 => 'Temporary Redirect',
-		400 => 'Bad Request',
-		401 => 'Unauthorized',
-		402 => 'Payment Required',
-		403 => 'Forbidden',
-		404 => 'Not Found',
-		405 => 'Method Not Allowed',
-		406 => 'Not Acceptable',
-		407 => 'Proxy Authentication Required',
-		408 => 'Request Timeout',
-		409 => 'Conflict',
-		410 => 'Gone',
-		411 => 'Length Required',
-		412 => 'Precondition Failed',
-		413 => 'Request Entity Too Large',
-		414 => 'Request-URI Too Long',
-		415 => 'Unsupported Media Type',
-		416 => 'Requested Range Not Satisfiable',
-		417 => 'Expectation Failed',
-		500 => 'Internal Server Error',
-		501 => 'Not Implemented',
-		502 => 'Bad Gateway',
-		503 => 'Service Unavailable',
-		504 => 'Gateway Timeout',
-		505 => 'HTTP Version Not Supported'
-		);
-	// Set HTTP Response
-	header('HTTP/1.1 '.$response['status'].' '.$http_response_code[ $response['status'] ]);
-	// Set HTTP Response Content Type
-	header('Content-Type: application/json; charset=utf-8');
-	// Format data into a JSON response
-	$json_response = json_encode($response['data']);
-	// Deliver formatted data
-	echo $json_response;
-	exit;
-}
-// Set default HTTP response of 'Not Found'
-$response['status'] = 404;
-$response['data'] = NULL;
-$url_array = explode('/', $_SERVER['REQUEST_URI']);
-array_shift($url_array); // remove first value as it's empty
-// remove 2nd and 3rd array, because it's directory
-array_shift($url_array); // 2nd = 'NativeREST'
-array_shift($url_array); // 3rd = 'api'
-// get the action (resource, collection)
-$action = $url_array[0];
-// get the method
-$method = $_SERVER['REQUEST_METHOD'];
+header("Content-type:application/json");
+$dbServername = '147.232.40.14:3306';
+$dbUsername = "km863qc";
+$password = "km863qc";
+$connect = mysqli_connect($dbServername,$dbUsername,$password,$dbUsername);
+$sql = "SELECT Miestnost.id as ID, Miestnost.label as Miestnosti, Regal.label as Regale, Miesto.label as Miesta, Polozka.nazov as Polozka, Polozka.nakupna_cena as Nakupna_cena, Polozka.predajna_cena as Predajna_cena, Polozka.balenie as Balenie, Polozka.mnozstvo as Mnozstvo,Vlastnost.nazov as Vlastnosti, Vlastnost.popis as Popis from Miestnost inner JOIN Regal on Miestnost.id = Regal.id_miestnost inner join Miesto on Regal.id = Miesto.id_regal inner join Polozka on Miesto.id = Polozka.id_miesto left join Vlastnost on Vlastnost.id_polozka = Polozka.id";
 
-deliver_response($response);
+
+
+$result = mysqli_query($connect,$sql);
+    $resultcheck = mysqli_num_rows($result);
+    if ($resultcheck>0){
+        while ($row = mysqli_fetch_assoc($result)){
+            $jsondata[]=$row;
+        }
+    }
+
+$jsondata = json_encode($jsondata);
+print_r($jsondata);
+$connect->close();
 ?>
